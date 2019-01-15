2cc3836fa53ef2aa46dc6ff2ddad18f7e93fcff7
unknown <veli.ozturk@gmail.com>
2019-01-15 12:25:46 +0200

    graal

--- src/util/RedisUtil.java
+++ src/util/RedisUtil.java
@@ -6,37 +6,74 @@ import java.util.Map;
 import redis.clients.jedis.Jedis;
 
 public class RedisUtil {
-	private static Map<String, Jedis> redisMap = new HashMap<>(); 
+	private static Map<String, Jedis> redisHostMap = new HashMap<>(); 
 	
+	private String host = null;
+	private Jedis jedis = null;
+	
+	
+	public Jedis getJedis() {
+		return jedis;
+	}
+
+
+	public RedisUtil(String host, String userName, String passWord) {
+		super();
+		this.host = host;
+		jedis = redisHostMap.get(host);
+		if(jedis!=null){
+			if(!jedis.isConnected()){
+				redisHostMap.remove(host);
+				try{jedis.close();}catch(Exception ee){}				
+			} else return;
+		}
+		jedis = new Jedis(host);
+		redisHostMap.put(host, jedis);
+	}
+
+
 	public static Jedis getConnection(String host){
-		Jedis j = redisMap.get(host);
+		Jedis j = redisHostMap.get(host);
 		if(j!=null){
 			if(!j.isConnected()){
+				redisHostMap.remove(host);
 				try{j.close();}catch(Exception ee){}				
 			} else return j;
 		}
 		j = new Jedis(host);
-		redisMap.put(host, j);
+		redisHostMap.put(host, j);
 		return j;		
 	}
 	
-	public static void closeAll(){
-	}
 	
 	public static void close(String host){
-		Jedis j = redisMap.get(host);
+		Jedis j = redisHostMap.get(host);
 		if(j!=null){
 			if(!j.isConnected()){
+				redisHostMap.remove(host);
 				try{j.close();}catch(Exception ee){}				
 			}
 		}		
 	}
+	public void close(){
+		if(jedis!=null){
+			if(!jedis.isConnected()){
+				redisHostMap.remove(host);
+				try{jedis.close();}catch(Exception ee){}				
+			}
+		}		
+	} 
 	
 	public static String put(String host, String k, String v){
 		Jedis j = getConnection(host);
 		return j.set(k, v);		
 	}
 	
+	/*
+	public static String add(String host, String k, String v){
+		Jedis j = getConnection(host);
+		return j.(k, v);		
+	}*/
 	
 	public static long rpush(String host, String k, String v){
 		Jedis j = getConnection(host);
@@ -49,14 +86,19 @@ public class RedisUtil {
 	}
 	
 	
-	public static String rpop(String host, String k, String v){
+	public static String rpop(String host, String k){
 		Jedis j = getConnection(host);
 		return j.rpop(k);		
 	}
 	
-	public static String lpop(String host, String k, String v){
+	public static String lpop(String host, String k){
 		Jedis j = getConnection(host);
 		return j.lpop(k);		
+	}
+	
+	public static long llen(String host, String k){
+		Jedis j = getConnection(host);
+		return j.llen(k);		
 	}
 	
 	public static String get(String host, String k){
@@ -70,4 +112,10 @@ public class RedisUtil {
 		return j.info(section);	
 	}
 	
+	public static void closeAll(){
+		for(Jedis j:redisHostMap.values())if(j!=null && j.isConnected())try{
+			j.close();
+		}catch(Exception ee){}
+		redisHostMap.clear();
+	}
 }
